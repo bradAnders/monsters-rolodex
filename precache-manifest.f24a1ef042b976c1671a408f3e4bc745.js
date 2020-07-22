56b5efb7fb9b9dc4c796fbc41611a1c90da6dbb0
Brad Anderson <btanders@pm.me>
2020-07-21 18:51:14 -0700

    Updates

---
+++ precache-manifest.f24a1ef042b976c1671a408f3e4bc745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54df48c46b0ac92ef2af50bc71007ed1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a04bdd072e3ba0e9b744",
+    "url": "/monsters-rolodex/static/css/main.4b0d02cb.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04bdd072e3ba0e9b744",
+    "url": "/monsters-rolodex/static/js/main.0596e7a1.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
